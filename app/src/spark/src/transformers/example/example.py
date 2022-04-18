e319f2ea36b90ae442cac3f3e15e1a3aad4d8c7b
pepe_fabra_valverde <jofaval@outlook.com>
2022-04-18 23:37:48 +0200

    Define the new example

--- app/src/spark/src/transformers/example/example.py
+++ app/src/spark/src/transformers/example/example.py
@@ -1,8 +1,16 @@
 from parent import SparkTransformer
 from .models import TwitterResponse, Tweet, User
 
+from module.spark.normalization import minmax_normalization
+
 class ExampleSparkTransformer(SparkTransformer):
     """Example Spark Transformer"""
+    def on_clean(self) -> None:
+        self.df = self.df.dropna()
+
+    def on_normalize(self) -> None:
+        self.df = minmax_normalization(self.df)
+
     pass
 
 if __name__ == '__main__':
