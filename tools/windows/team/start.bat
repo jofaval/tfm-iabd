c33b4d0980e67f601273acc1398cfee5c3007b5a
pepe_fabra_valverde <jofaval@outlook.com>
2022-04-20 23:52:54 +0200

    Fix minor mistake

--- tools/windows/team/start.bat
+++ tools/windows/team/start.bat
@@ -5,7 +5,9 @@ REM Starts all of the services
 REM Start the Frontend service
 cd frontend/src/scripts/windows
 start start.bat
+cd ../../../
 
 REM Start the Backend service
 cd backend/src/scripts/windows
 start start.bat
+cd ../../../
